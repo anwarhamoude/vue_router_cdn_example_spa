837f49f7a237688195c8143566402aff2b0b0476
Anwar Hamoude <newreliancemedia@gmail.com>
2019-07-28 07:21:59 -0700

    added not found page

---
+++ views/not_found.js
@@ -0,0 +1,15 @@
+var NotFound = Vue.component('not_found',{
+  template:`<div class="pages">
+        <div class="container">
+        <div class="center">
+        <h3>Whoops...</h3>
+        <br/>
+        <h1>404</h1>
+        <br>
+        <h3>Component Not Found</h3>
+        <br>
+        <h4>This Route Does Not Exist</h4>
+        </div>
+        </div>
+  </div>`
+})
